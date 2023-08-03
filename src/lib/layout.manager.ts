3dba322092d0dab73e95439da412d2bff9b80822
Sean Gravener <sean@gravener.net>
2023-08-02 20:35:08 -0400

    Refactor layout manager

--- src/lib/layout.manager.ts
+++ src/lib/layout.manager.ts
@@ -1,54 +1,78 @@
 import { Scene } from "phaser";
-import { SpriteDefinition } from "../objects/base.sprite";
 import { PlayerSprite } from "../objects/player.sprite";
+
+interface Bounds {
+  cell: { width: number, height: number };
+  rows: number;
+  cols: number;
+}
+
+interface Config {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  rows: number;
+  cols: number;
+}
 
 export class LayoutManager {
   scene: Scene;
-  config: any;
+  config: Config;
   graphics: any;
-
-  get bounds() {
-    const { x, y, width, height, rows, cols } = this.config;
-
-    return {
-      cell: { width: width / cols, height: height / rows },
-      rows,
-      cols,
-    };
-  }
 
   constructor({ scene, x = 0, y = 0, rows = 12, cols = 12 }: any) {
     this.scene = scene;
     const { width, height } = this.scene.cameras.main;
     this.config = { x, y, width, height, rows, cols };
-
     this.addGrid();
   }
 
-  addGrid() {
-    const { x, y, width, height, rows, cols } = this.config;
+  get bounds(): Bounds {
+    return {
+      cell: { 
+        width: this.config.width / this.config.cols, 
+        height: this.config.height / this.config.rows 
+      },
+      rows: this.config.rows,
+      cols: this.config.cols,
+    };
+  }
 
+  addGrid(): void {
     this.graphics = this.scene.add.graphics({ x: 0, y: 0, z: 99 });
     this.graphics.lineStyle(1, 0xff00ff, 0.33);
+    this.drawGrid();
+  }
 
+  drawGrid(): void {
     this.graphics.beginPath();
-    for (let i = 0; i < width; i += this.bounds.cell.width) {
-      this.graphics.moveTo(i, 0);
-      this.graphics.lineTo(i, height);
-    }
-    for (let i = 0; i < height; i += this.bounds.cell.height) {
-      this.graphics.moveTo(0, i);
-      this.graphics.lineTo(width, i);
-    }
+    this.drawHorizontalLines();
+    this.drawVerticalLines();
     this.graphics.closePath();
     this.graphics.strokePath();
   }
 
-  place(x: number, y: number, sprite: PlayerSprite) {
-    // this.scene.coordinates;
-    const x2 = this.bounds.cell.width * x + this.bounds.cell.width / 2;
-    const y2 = this.bounds.cell.height * y + this.bounds.cell.height / 2;
-    sprite.x = x2;
-    sprite.y = y2;
+  drawHorizontalLines(): void {
+    for (let i = 0; i < this.config.width; i += this.bounds.cell.width) {
+      this.graphics.moveTo(i, 0);
+      this.graphics.lineTo(i, this.config.height);
+    }
+  }
+
+  drawVerticalLines(): void {
+    for (let i = 0; i < this.config.height; i += this.bounds.cell.height) {
+      this.graphics.moveTo(0, i);
+      this.graphics.lineTo(this.config.width, i);
+    }
+  }
+
+  place(x: number, y: number, sprite: PlayerSprite): void {
+    sprite.x = this.calculateSpriteCoordinate(x, this.bounds.cell.width);
+    sprite.y = this.calculateSpriteCoordinate(y, this.bounds.cell.height);
+  }
+
+  calculateSpriteCoordinate(index: number, cellDimension: number): number {
+    return cellDimension * index + cellDimension / 2;
   }
 }
